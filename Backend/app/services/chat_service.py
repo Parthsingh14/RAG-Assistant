197b0b3d72673c63eb70b1e284fcda7f7cdbf937
Parth Singh <parthsingh503@gmail.com>
2026-09-21 17:43:08 +0530

    updated chat_service for integrating the graph with the api

--- Backend/app/services/chat_service.py
+++ Backend/app/services/chat_service.py
@@ -1,45 +1,19 @@
-from app.services.retrieval_service import RetrievalService
-from app.rag.llm.llm_service import LLMService
-from app.rag.prompts.rag_prompt import RAG_PROMPT
+from app.graph.graph import create_graph
 
-
-def build_context(results):
-    context_parts = []
-    sources = []
-
-    for index,result in enumerate(results, start=1):
-        text = result.get('text')
-
-        if text:
-            context_parts.append(f"--- Document Chunk {index} ---\n{text}")
-
-        sources.append({
-            "filename": result.get('filename'),
-            "page_number": result.get('page_number')
-        })
-
-
-    context = "\n\n".join(context_parts)
-    return context, sources
 
 class ChatService:
     def __init__(self):
-        self.retrieval_service = RetrievalService()
-        self.llm_service = LLMService()
+        self.graph = create_graph()
 
     def get_answer(self,question:str):
 
-        results = self.retrieval_service.retrieve(question=question, top_k=3)
-        context, sources = build_context(results)
-
-        prompt = RAG_PROMPT.invoke({
-            "context": context,
-            "question": question
+        results = self.graph.invoke({
+            "question": question,
+            "retry_count": 0
         })
 
-        response = self.llm_service.generate(prompt)
 
         return {
-            "answer": response.content,
-            "sources": sources
+            "answer": results["answer"],
+            "sources": results.get("sources", [])
         }
